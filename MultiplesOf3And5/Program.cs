8427903582b352452a5354436c5eda82f00c6b55
Jake Callcut <jakecallcut123@gmail.com>
2020-04-22 19:25:24 +0100

    completed problem one

---
+++ MultiplesOf3And5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MultiplesOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem 1, project euler, Multiples of 3 and 5
+            int counter = 0;
+            int total = 0;
+
+            do
+            {
+                if (counter % 3 == 0)
+                {
+                    total = total + counter;
+                    counter++;
+                }
+                else if (counter % 5 == 0)
+                {
+                    total = total + counter;
+                    counter++;
+                }
+                else
+                {
+                    counter++;
+                }
+            } while (counter < 1000);
+
+            Console.WriteLine(total);
+        }
+    }
+}
