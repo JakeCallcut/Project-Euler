8d213b0719a35e1b9291052bf7100401692d5be1
Jake Callcut <jakecallcut123@gmail.com>
2020-04-23 16:08:06 +0100

    solved problem 6

--- SumSquareDifference.py
+++ SumSquareDifference.py
@@ -3,13 +3,13 @@ import os
 import sys
 import math
 
-#Problem 4, Project Euler, Sum Square Difference
+#Problem 6, Project Euler, Sum Square Difference
 
 #!Square of sum
 num = 0
 total = 0
 
-for num in range (1, 100):
+for num in range (1, 101):
     total = total + num
 else:
     squaretotal = total * total
@@ -21,10 +21,10 @@ total = 0
 
 #!Sum of Square
 
-for num in range (1,100):
+for num in range (1, 101):
     squarenum = num * num
     total = total + squarenum
 else:
     print(total)
 
-print(total - squaretotal)
+print(squaretotal - total)
