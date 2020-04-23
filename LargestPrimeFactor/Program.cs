0d196074df074b8cf28106a0e161763962bbe9a9
Jake Callcut <jakecallcut123@gmail.com>
2020-04-23 13:24:40 +0100

    Worked on problem 3

---
+++ LargestPrimeFactor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LargestPrimeFactor
+{
+    class Program
+    {   
+        //Problem 3, Project Euler, Largest Prime Factor
+        const long number = 600851475143;
+        static public bool IsPrime(long x)
+        {
+            for (long i = 2; i < x; i++)
+            {
+                if(x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            //number == 600851475143
+            long HPF = 1;
+
+            for (long i = 2; i <= number; i++)
+            {
+                if (Program.IsPrime(i) == true)
+                {
+                    if(i > HPF)
+                    {
+                        HPF = i;
+                    }
+                }
+            }
+            Console.WriteLine(HPF);
+            Console.ReadKey();
+        }
+    }
+}
