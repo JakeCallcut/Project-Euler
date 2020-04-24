c9bcd00103586cc6ae4849a4d720eea696290478
Jake Callcut <jakecallcut123@gmail.com>
2020-04-24 10:37:00 +0100

    solved problem 7

---
+++ 1000-DigitFibonacciNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1000_DigitFibonacciNumber
+{
+    class Program
+    {   
+        //Problem 25, Project Euler, 1000-Digit Fibonacci Sequence
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ 10001stPrime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _10001stPrime
+{
+    class Program
+    {
+        //Problem 7, Project Euler, 10001st Prime
+        static void Main(string[] args)
+        {
+            static bool IsPrime(int x)
+            {
+                for (long i = 2; i < x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            int noofprimes = 0;
+            int i = 2;
+            int lastprime = 2;
+            do
+            {
+                if (IsPrime(i) == true)
+                {
+                    noofprimes++;
+                    lastprime = i;
+                }
+                i++;
+            } while (noofprimes < 10001);
+
+            Console.WriteLine(lastprime);
+            Console.WriteLine(i);
+            Console.WriteLine(noofprimes);
+            Console.ReadKey();
+        }
+    }
+}
